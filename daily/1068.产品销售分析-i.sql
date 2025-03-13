43a0229db50ce74fc15513a940dce0d2d901e4c5
xjh041110 <2791158256@qq.com>
2025-03-13 10:22:13 +0800

    leetcode

---
+++ daily/1068.产品销售分析-i.sql
@@ -0,0 +1,13 @@
+--
+-- @lc app=leetcode.cn id=1068 lang=mysql
+--
+-- [1068] 产品销售分析 I
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT product_name, year, price
+FROM Sales s JOIN product p
+ON s.product_id = p.product_id
+-- @lc code=end
+
